19de79b702e51d2f2ae32025e280f5ca24a5a858
annaz8 <zhong2721@gmail.com>
2015-09-16 08:16:57 -0700

    Changed code

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-void setup()
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+
+public void setup()
 {
 	size(400,400);
 	noLoop();
 	background(0);
 }
-void draw()
+public void draw()
 {
 	background(0);
 	int total = 0;
@@ -26,7 +42,7 @@ void draw()
 	text("The current total is " + total, 20, 25);
 
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -41,12 +57,12 @@ class Die
 		myY = y;
 
 	}
-	void roll()
+	public void roll()
 	{
 		numRolled = ((int)(Math.random()*6)+1);
 		
 	}
-	void show()
+	public void show()
 	{
 		stroke(0);
 		fill(255);
@@ -99,4 +115,13 @@ class Die
 		}
 
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
